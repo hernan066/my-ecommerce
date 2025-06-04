58dc43ff3b1ff52d9ebf50bb4e2eb7b9d77d183e
Hernan <hernan0066@gmail.com>
2025-06-04 20:17:31 -0300

    feat: update TypeScript paths and Vitest configuration for backend and test directories

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,12 +5,14 @@ import path from "path";
 export default defineConfig({
   test: {
     globals: true,
-    include: ["apps/backend/test/**/*.test.ts"],
     environment: "node",
+    include: ["apps/backend/test/**/*.test.ts", "domain/test/**/*.test.ts"],
   },
   resolve: {
     alias: {
-      "@domain": path.resolve(__dirname, "./domain/src"),
+      "@domain": path.resolve(__dirname, "domain/src"),
+      "@backend": path.resolve(__dirname, "apps/backend/src"),
+      "@test": path.resolve(__dirname, "apps/backend/test"),
     },
   },
 });
