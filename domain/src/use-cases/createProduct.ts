bce288db9afb353e3ebb02b304bf94658f747b72
Hernan <hernan0066@gmail.com>
2025-06-04 23:37:30 -0300

    feat(domain): implement CreateProduct use case with asynchronous product creation

--- domain/src/use-cases/createProduct.ts
+++ domain/src/use-cases/createProduct.ts
@@ -1,8 +1,18 @@
-import { Product } from "../entities/Product";
+import { ProductRepository } from "@domain/repositories/product-repository";
+import { Product } from "@domain/entities/Product";
 import { randomUUID } from "crypto";
+// import { validateName, validatePrice } from "./validators";
 
 export class CreateProduct {
-  execute({ name, price }: { name: string; price: number }): Product {
-    return new Product({ id: randomUUID(), name, price });
+  constructor(private readonly productRepo: ProductRepository) {}
+
+  async execute({ name, price }: { name: string; price: number }) {
+    /*  validateName(name);
+    validatePrice(price); */
+
+    const product = new Product({ id: randomUUID(), name, price });
+    await this.productRepo.create(product);
+
+    return product;
   }
 }
